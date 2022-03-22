b715c0907cbc8cc836724de3397a762ed01ff5f5
Léo Genot <45610291+leogenot@users.noreply.github.com>
2022-03-22 17:49:35 +0100

    filter by alcohol

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,13 @@
   <div id="app">
     <section>
       <div class="list-options">
-        <input
-          type="text"
-          v-model="search"
-          placeholder="Type a beer name"
-        />
+        <input type="text" v-model="search" placeholder="Type a beer name" />
         <label for="beer-sort">Sort by : </label>
         <select v-model="beersSortType" id="beer-sort">
           <option value="AZName">Sort from A to Z</option>
           <option value="ZAName">Sort from Z to A</option>
+          <option value="PlusABV">Sort from least alcohol</option>
+          <option value="MinusABV">Sort from most alcohol</option>
         </select>
       </div>
       <div v-if="!beers.length" class="loading">Loading...</div>
@@ -62,29 +60,36 @@ export default {
     this.addBeer();
     this.addBeer();
   },
-    computed: {
+  computed: {
     beersOrganizationData() {
       const field = ["AZName", "ZAName"].includes(this.beersSortType)
         ? "name"
         : "abv";
-      const reversed = ["ZAName"].includes(this.beersSortType);
+      const reversed = ["ZAName", "MinusABV"].includes(this.beersSortType);
       const filterFunc = (a) =>
         a.name.toLowerCase().includes(this.search.toLowerCase());
-      const comparator = (a, b) => a[field].localeCompare(b[field]);
+      let comparator = undefined
+      if (field == "name") {
+        comparator = (a, b) => a.name.localeCompare(b.name);
+      } else if (field == "abv") {
+        comparator = (a, b) => parseFloat(a.abv) - parseFloat(b.abv);
+      }
+
       let data = this.beers.filter(filterFunc);
       data = data.sort(comparator);
       if (reversed) data = data.reverse();
       return data;
-    }
+    },
   },
   methods: {
+    /*
     bottomVisible() {
       const scrollY = window.scrollY;
       const visible = document.documentElement.clientHeight;
       const pageHeight = document.documentElement.scrollHeight;
       const bottomOfPage = visible + scrollY >= pageHeight;
       return bottomOfPage || pageHeight < visible;
-    },
+    }, */
     addBeer() {
       axios.get("https://api.punkapi.com/v2/beers/random").then((response) => {
         let api = response.data[0];
@@ -102,11 +107,7 @@ export default {
           this.addBeer();
         } */
       });
-      
     },
-    
   },
 };
 </script>
-
-
